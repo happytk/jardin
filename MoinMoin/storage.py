9981354fdca932e05c204290868c02fec34a9103
happytk <rainyblue@gmail.com>
2018-03-17 16:20:16 +0900

    git-quotation-encoding

--- MoinMoin/storage.py
+++ MoinMoin/storage.py
@@ -193,6 +193,7 @@ class GitMiddleware(Middleware):
                     result = editlog.EditLogLine(_usercache)
                     if entry[0] == '"' and entry[-1] == '"':
                         entry = entry[1:-1]  # git quotation
+                        entry = entry.decode('utf8')
                     if entry.endswith('.md'):
                         # result.pagename = wikiutil.unquoteWikiname(entry[:-3])
                         result.pagename = entry[:-3]
